5c607e3abde4886ed82f319916182162496ae38b
clucle <wjdenwls123@gmail.com>
2017-02-11 16:28:41 +0900

    feat: client module
    new class for client

--- multi_client/client/client/client.cpp
+++ multi_client/client/client/client.cpp
@@ -1,41 +1,21 @@
-#include<winsock2.h>
-#include<WS2tcpip.h>
 #include<stdio.h>
 #include<iostream>
 #include<string>
 
-#pragma comment(lib,"Ws2_32.lib")
+#include "client.h"
 
-#define PORT "1234"
-#define BUFFERSIZE 512
-
-
-struct addrinfo *result = NULL, ref;
 
 int main(){
-    WSADATA wsadata;
-    WSAStartup(MAKEWORD(2, 2), &wsadata);
+    Client cli;
 
-    ZeroMemory(&ref, sizeof(ref));
-    ref.ai_family = AF_INET;
-    ref.ai_protocol = IPPROTO_TCP;
-    ref.ai_flags = AI_PASSIVE;
-    getaddrinfo("127.0.0.1", PORT, &ref, &result);
-
-    SOCKET Ani = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    connect(Ani, result->ai_addr, result->ai_addrlen);
-    int status;
     std::string sendMsg;
     do
     {
         std::getline(std::cin, sendMsg);
-        status = send(Ani, sendMsg.c_str(), sendMsg.length(), 0);
+        cli.SendMsg(sendMsg);
 
-    } while (status!=SOCKET_ERROR);
+    } while (true);
 
-    shutdown(Ani, SD_SEND);
-    closesocket(Ani);
-    WSACleanup();
     return 0;
 }
 
